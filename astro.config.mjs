385f3a317231cddaf603de4cfdeda34ed3da92b0
RNZ <andrei.tanase.m@gmail.com>
2024-05-09 02:13:55 +0300

    added gzip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rnz.ro',
-  integrations: [tailwind(), sitemap(), partytown()]
+  integrations: [tailwind(), sitemap(), partytown(), playformCompress()]
 });
